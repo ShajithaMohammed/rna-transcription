a8980bf8141c94cf8fd04b914d5fd90f5123c46d
ShajithaMohammed <shajithaparveen28@gmail.com>
2019-07-06 17:12:12 +0530

    [Sajitha] Moving HashMap setUp into constructor.

--- src/main/java/RnaTranscription.java
+++ src/main/java/RnaTranscription.java
@@ -3,17 +3,25 @@ import java.util.HashMap;
 
 class RnaTranscription {
 
+    private Map<Character, Character> conversionFactor = new HashMap<Character, Character>();
+
+    public RnaTranscription() {
+        conversionFactor.put('G', 'C');
+        conversionFactor.put('C', 'G');
+        conversionFactor.put('T', 'A');
+        conversionFactor.put('A', 'U');
+    }
+
     String transcribe(String dnaStrand) {
-        Map<Character,Character> conversionFactor=new HashMap<Character,Character>();
-        conversionFactor.put('G','C');
-        conversionFactor.put('C','G');
-        conversionFactor.put('T','A');
-        conversionFactor.put('A','U');
-        String rnaStrand="";
-        for(int i=0;i<dnaStrand.length();i++){
-           rnaStrand = rnaStrand +""+ conversionFactor.get(dnaStrand.charAt(i));
+
+        String rnaStrand = "";
+        
+        for (int i = 0; i < dnaStrand.length(); i++) {
+            rnaStrand = rnaStrand + "" + conversionFactor.get(dnaStrand.charAt(i));
         }
-     return rnaStrand;
+
+        return rnaStrand;
+
     }
 
 }
